13f216094969a3855ce26f9f18155b303dae6192
Muneer <muneer.raza098@gmail.com>
2024-05-11 17:18:46 +0500

    disabel loading bar

--- dataset/data_loader.py
+++ dataset/data_loader.py
@@ -1,8 +1,8 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
-from tqdm import tqdm
+tfds.disable_progress_bar()
 
-from .data_preprocessing import DataPreprocessor
+from data_preprocessing import DataPreprocessor
 
 class DataLoader:
     def __init__(self, dataset_name, splits, img_size, batch_size, clip_min, clip_max):
@@ -21,17 +21,32 @@ class DataLoader:
         datapreprocessor = DataPreprocessor(self.img_size, self.clip_min, self.clip_max)
 
         train_preprocessing = lambda x: datapreprocessor.train_preprocessing(x)
-        
-        # Apply preprocessing to the dataset
-        ds = ds.map(train_preprocessing, num_parallel_calls=tf.data.AUTOTUNE)
-        
-        # Calculate the number of batches in the dataset
-        num_batches = tf.data.experimental.cardinality(ds).numpy() // self.batch_size
-
-        # Apply batching, shuffling and prefetching
-        train_ds = ds.batch(self.batch_size, drop_remainder=True).shuffle(self.batch_size * 2).prefetch(tf.data.AUTOTUNE)
-
-        # Wrap the train_ds with tqdm for progress bar
-        train_ds = tqdm(train_ds, total=num_batches)
+        train_ds = (
+            ds.map(train_preprocessing, num_parallel_calls=tf.data.AUTOTUNE)
+            .batch(self.batch_size, drop_remainder=True)
+            .shuffle(self.batch_size * 2)
+            .prefetch(tf.data.AUTOTUNE)
+        )
 
         return train_ds
+    
+
+if __name__ == "__main__":
+    dataset_name = "cifar10"
+    splits = ["train"]
+    batch_size = 64
+    img_size = 32
+    clip_min = -1.0
+    clip_max = 1.0
+
+    dataloader = DataLoader(
+        dataset_name=dataset_name,
+        splits=splits,
+        img_size=img_size,
+        batch_size=batch_size,
+        clip_min=clip_min,
+        clip_max=clip_max,
+    )
+
+    train_ds = dataloader.load_data()
+    print(train_ds)
